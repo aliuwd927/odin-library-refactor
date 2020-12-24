704408c1e64b04bb82c117b98e37049dc33d9f8f
Andy Liu <aliu927@gmail.com>
2020-12-23 22:42:05 -0800

    html css

---
+++ js/index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/index.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="topContainer">
+                <button class='addBookBtn'>Add Book</button>
+                <form class='formContainer'>
+                    <label for="title">Title of book</label><br>
+                    <input type="text" class='title'><br>
+                    <label for="author">Author of book</label><br>
+                    <input type="text" class='author'><br>
+                    <button class='addBook'>Submit</button>
+                </form>
+            </div>
+
+            <div class="bottomContainer">
+                    <!--
+                     <div class='booksAdded'>
+                         ${books.title}<br>
+                                ${books.author}
+                     </div>
+                    -->
+            </div>
+        </div>  
+        <script src="./js/index.js" async defer></script>
+    </body>
+</html>
